909a0362496e885394f8bdaaf79eeedaef76b795
xacsel01 <66441489+xacsel01@users.noreply.github.com>
2020-06-11 01:19:02 -0400

    Add files via upload

---
+++ calculadora ejercicio.py
@@ -0,0 +1,9 @@
+print("Hola, bienvenido a la calculadora eficiente, ingresa un numero")
+numero1 = int(input())
+numero2 = int(input("ingrese el segundo numero "))
+
+
+print(" el resultado sumado es: ",numero1+numero2)
+print(" el resultado restado es: ",numero1-numero2)
+print(" el resultado multiplicado es: ",numero1*numero2)
+print(" el resultado dividido es: ",numero1/numero2)
